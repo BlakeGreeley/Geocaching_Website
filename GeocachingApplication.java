da8d1077cafd0871809fbcd01dcd9175aeaaf973
Blake Greeley <98073180+BlakeGreeley@users.noreply.github.com>
2022-09-24 16:37:58 -0700

    Add files via upload

---
+++ GeocachingApplication.java
@@ -0,0 +1,13 @@
+package com.personal.geocaching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeocachingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeocachingApplication.class, args);
+	}
+
+}
